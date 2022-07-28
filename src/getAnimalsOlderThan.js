fa26af220bd4c047e76c2014a18f057789f77922
kayckhirt <kayckhirt15@gmail.com>
2022-07-28 18:40:07 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+
 function getAnimalsOlderThan(animal, age) {
+  return species.find((especie) => especie.name === animal)
+    .residents.every((idade) => idade.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
