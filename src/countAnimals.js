c4933413992119bc06cc3612d93a91b1698612eb
kayckhirt <kayckhirt15@gmail.com>
2022-07-29 15:17:44 -0300

    Requisito 6

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = require('../data/zoo_data');
 
+function countAnimals(animal) {
+ const animais = species.filter((nome)=> nome.name === animal)
+ .map((resident)=> resident.residents);
+ const contaAnimais = animais.reduce((resultadoParcial,especie) => {
+  if (resultadoParcial[especie] === undefined) {
+    resultadoParcial[especie] = 1
+ }else {
+  resultadoParcial[especie] +=1
+ }
+ return resultadoParcial;
+}, {})
+return contaAnimais;
+}
 module.exports = countAnimals;
+
+
+console.log(contaAnimais('lions'))
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,42 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se Count dá 4', () => {
+    expect(handlerElephants('count')).toBe(4)
+  })
+  it('Verifica se o (names) não retorna um array vazio', () => {
+    expect(handlerElephants('names')).not.toEqual([]);
+  })
+  it('Verifica se os nomes correspondem ao names', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  })
+  it('Verifica se Elephants o id corresponde ao mesmo', () => {
+    expect(handlerElephants('id')).toBe('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
+  })
+  it('Verifica se a popularidade não é igual a 1', () => {
+    expect(handlerElephants('popularity')).not.toEqual(1);
+  })
+  it('Verifica se a popularidade é igual a 5', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  })
+  it('Verifica se a availability', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+    expect(handlerElephants('availability')).not.toEqual(['Wednesday','Monday']);
+  })
+  it('Verifica se a localizacao corresponde ao local certo', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+    expect(handlerElephants('location')).not.toEqual('N');
+  })
+  it('Verifica se idade media dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  })
+  it('Verifica se Elephants recebe parâmetro null', () => {
+    expect(handlerElephants('tamanhoDaOrelha')).toEqual(null);
+  })
+  it('Verifica se Elephants recebe undefined', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  })
+  it('Verifica se Elephants recebe array vazio e retorna mensagem de parâmetro invalido', () => {
+    expect(handlerElephants([])).toEqual("Parâmetro inválido, é necessário uma string");
+  })
+});
