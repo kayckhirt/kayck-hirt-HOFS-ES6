a4434b09e27177d69975a57a779106f3da7eb5e9
kayckhirt <kayckhirt15@gmail.com>
2022-07-29 15:47:45 -0300

    fix lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,21 +1,18 @@
 const data = require('../data/zoo_data');
 
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
- const animais = species.filter((nome)=> nome.name === animal)
- .map((resident)=> resident.residents);
- const contaAnimais = animais.reduce((resultadoParcial,especie) => {
-  if (resultadoParcial[especie] === undefined) {
-    resultadoParcial[especie] = 1
- }else {
-  resultadoParcial[especie] +=1
- }
- return resultadoParcial;
-}, {})
-return contaAnimais;
+//  const animais = species.filter((nome)=> nome.name === animal)
+//  .map((resident)=> resident.residents);
+//  const contaAnimais = animais.reduce((resultadoParcial,especie) => {
+//   if (resultadoParcial[especie] === undefined) {
+//     resultadoParcial[especie] = 1
+//  }else {
+//   resultadoParcial[especie] +=1
+//  }
+//  return resultadoParcial;
+// }, {})
+// return contaAnimais;
 }
 module.exports = countAnimals;
-
-
-console.log(contaAnimais('lions'))
