7bf21cbfb72701373b9296c534a64a00059384c1
kayckhirt <kayckhirt15@gmail.com>
2022-07-29 10:21:11 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = require('../data/zoo_data');
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const funcionarios = employees.filter((funcionario) =>
+    (funcionario.managers.includes((managerId))));
+
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return funcionarios.map((criaArray) => `${criaArray.firstName} ${criaArray.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
