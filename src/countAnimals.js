305251cf2106cc1601c51d17764978f9b0cf5fa1
kayckhirt <kayckhirt15@gmail.com>
2022-08-01 16:07:44 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,20 @@
 const data = require('../data/zoo_data');
 
-// const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  } if (animal !== undefined) {
+    const animais = species.filter((nome) => nome.name === animal.specie);
+    if (animal.sex !== undefined) {
+      return animais[0].residents.filter((sexoDoAnimal) => sexoDoAnimal.sex === animal.sex).length;
+    }
+    return animais[0].residents.length;
+  }
 
 }
 module.exports = countAnimals;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const {species, hours} = require('../data/zoo_data');
 
+const semana = {
+  Tuesday: { officeHour:`Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm`, exhibition: []},
+  Wednesday: { officeHour:`Open from ${hours.Wednesday.open}am until ${hours.Wednesday.close}pm`, exhibition: []},
+  Thursday: { officeHour:`Open from ${hours.Thursday.open}am until ${hours.Thursday.close}pm`, exhibition: []},
+  Friday: { officeHour:`Open from ${hours.Friday.open}am until ${hours.Friday.close}pm`, exhibition: []},
+  Saturday: { officeHour:`Open from ${hours.Saturday.open}am until ${hours.Saturday.close}pm`, exhibition: []},
+  Sunday: { officeHour:`Open from ${hours.Sunday.open}am until ${hours.Sunday.close}pm`, exhibition: []},
+  Monday: { officeHour:`Open from ${hours.Monday.open}am until ${hours.Monday.close}pm`, exhibition: []},
+   
+}
+console.log(semana)
+function getSchedule(scheduleTarget) {
+  
+}
+console.log(getSchedule('penguins'))
 module.exports = getSchedule;
